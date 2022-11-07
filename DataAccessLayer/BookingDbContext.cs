f331ef77a5d6e47f15665a361abb3b3e3b3abc88
Valerio Caporali <cyndoriil@gmail.com>
2022-11-07 10:40:09 +0100

    Db Setting

---
+++ DataAccessLayer/BookingDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Booking_Exercise.Models.BookingModels;
+using Booking_Exercise.Models.HotelModels;
+using Booking_Exercise.Models.RatingModels;
+using Booking_Exercise.Models.RoomModels;
+using Booking_Exercise.Models.UserModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking_Exercise.DataAccessLayer
+{
+    public class BookingDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        public BookingDbContext(DbContextOptions<BookingDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ DataAccessLayer/DbDataAccessService.cs
@@ -0,0 +1,6 @@
+﻿namespace Booking_Exercise.DataAccessLayer
+{
+    public class DbDataAccessService
+    {
+    }
+}
